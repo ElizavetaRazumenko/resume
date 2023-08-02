cb1db709d32b13fb568d27b34f1ff7bfcbf0c481
Elizaveta Razumenko <razumenko99@mail.ru>
2023-08-02 20:08:11 +0300

    feat: add information

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ function createElement(elem, classN, parent, content = '') {
     parent.append(div);
     return div;
 }
+
 const body_interior = createElement('div', 'body-interior', body);
 const myData = createElement('div', 'my-data', body_interior);
 const myPhoto = createElement('div', 'my-photo', myData);
 const myName = createElement('div', 'my-name', myData, 'elizaveta razumenko');
-const myProf = createElement('div', 'my-prof', myData, 'frontend developer');
+const myProf = createElement('div', 'my-prof', myData, 'frontend-developer');
 const main = createElement('main', 'main', body_interior);
-const skills = createElement('div', 'skills', main, 'skills:');
+const skillsEducWrapper = createElement('div', 'skills-ed-wrapper', main);
+const skills = createElement('div', 'skills', skillsEducWrapper, 'skills:');
 const skills_list = createElement('ul', 'skills-ol', skills);
-const shills_Array = ['html', 'css', 'javascript', 'typescript', 'react'];
+const shills_Array = ['HTML5', 'CSS3', 'Javascript', 'Typescript', 'Git', 'Webpack', 'React'];
 shills_Array.forEach((item) => { createElement('li', 'skill', skills_list, `${item}`)});
-const education = createElement('div', 'education', main, 'education:');
-const education_un = createElement('div', 'education-info-un', education, 'belarusian national technical university');
-const education_spec = createElement('div', 'education-info-spec', education, 'speciality: civil engineer');
+const educationWrapper = createElement('div', 'education-wrapper', skillsEducWrapper);
+const education = createElement('div', 'education', educationWrapper, 'education:');
+const education_un = createElement('div', 'education-info-un', education, 'Belarusian National Technical University');
+const education_spec = createElement('div', 'education-info-spec', education, 'Speciality: Civil engineer');
+const courses = createElement('div', 'education courses', educationWrapper, 'courses:');
+const courseSchoolJS = createElement('div', 'courses-school', courses, 'RSSchool: JS / Front-end');
+const courseSchoolReact = createElement('div', 'courses-school', courses, 'RSSchool: React');
+
